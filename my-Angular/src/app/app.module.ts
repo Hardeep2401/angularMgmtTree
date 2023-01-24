737133a96d5af7a0afb18b52f4191385564f5b2b
Hardeep2401 <hardeep.singh@lpinfotech.com>
2023-01-24 18:53:52 +0530

    add new pages

--- my-Angular/src/app/app.module.ts
+++ my-Angular/src/app/app.module.ts
@@ -3,10 +3,46 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountComponent } from './account/account.component';
+import { CrudComponent } from './crud/crud.component';
+import { LayoutComponent } from './layout/layout.component';
+import { PagesComponent } from './pages/pages.component';
+import { LoginComponent } from './account/login/login.component';
+import { RegisterComponent } from './account/register/register.component';
+import { ForgotPasswordComponent } from './account/forgot-password/forgot-password.component';
+import { AddComponent } from './crud/add/add.component';
+import { EditComponent } from './crud/edit/edit.component';
+import { AsideComponent } from './layout/aside/aside.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { OfferComponent } from './pages/offer/offer.component';
+import { OrderComponent } from './pages/order/order.component';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './user/admin/admin.component';
+import { UserAdminComponent } from './user/user-admin/user-admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountComponent,
+    CrudComponent,
+    LayoutComponent,
+    PagesComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+    AddComponent,
+    EditComponent,
+    AsideComponent,
+    FooterComponent,
+    HeaderComponent,
+    DashboardComponent,
+    OfferComponent,
+    OrderComponent,
+    UserComponent,
+    AdminComponent,
+    UserAdminComponent
   ],
   imports: [
     BrowserModule,
