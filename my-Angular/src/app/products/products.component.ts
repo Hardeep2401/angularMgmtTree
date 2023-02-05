5137c5e077b3a291c2030aecba56fb4d8f313622
HarDeep <hardeep.singh@lpinfotech.com>
2023-02-06 02:29:38 +0530

    injection services

--- my-Angular/src/app/products/products.component.ts
+++ my-Angular/src/app/products/products.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.services';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
-  title = 'Product List';
+  pageTitle = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
@@ -23,50 +24,12 @@ export class ProductsComponent implements OnInit {
     console.log('In setter : ', value);
     this.filteredProducts = this.performFilter(value);
   }
+
   filteredProducts: IProduct[] = [];
   products: IProduct[] = [
-    {
-      "productImage": 'https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
-      "productId": 1,
-      "productName": "Apple Laptop",
-      "productCode": "GDN-0012",
-      "releaseDate": "March 10,2023",
-      "description": "Laptop",
-      "price": 1100.00,
-      "starRating": 4.5
-    },
-    {
-      "productImage": 'https://images.unsplash.com/photo-1610230099906-7ad016cf5cf9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-      "productId": 2,
-      "productName": "Apple Watch",
-      "productCode": "GDN-0012",
-      "releaseDate": "March 10,2023",
-      "description": "Watch",
-      "price": 700.00,
-      "starRating": 4.1
-    },
-    {
-      "productImage": 'https://images.unsplash.com/photo-1610230099906-7ad016cf5cf9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-      "productId": 3,
-      "productName": "Samsung Mobile",
-      "productCode": "GDN-0012",
-      "releaseDate": "March 10,2023",
-      "description": "Watch",
-      "price": 700.00,
-      "starRating": 4.1
-    },
-    {
-      "productImage": 'https://images.unsplash.com/photo-1609692814858-f7cd2f0afa4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-      "productId": 4,
-      "productName": "Iphone Apple",
-      "productCode": "GDN-0012",
-      "releaseDate": "March 10,2023",
-      "description": "Mobile Product",
-      "price": 455.00,
-      "starRating": 4.7
-    }
+    
+  ];
 
-  ]
   displayVal: string = '';
   getValue(val: string) {
     console.log(val);
@@ -75,24 +38,24 @@ export class ProductsComponent implements OnInit {
   addValue(val: string) {
     this.displayVal = val;
   }
+  constructor(private productService: ProductService) {}
 
   performFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLocaleLowerCase();
     return this.products.filter((product: IProduct) =>
-      product.productName.toLocaleLowerCase().includes(filterBy));
-
+      product.productName.toLocaleLowerCase().includes(filterBy)
+    );
   }
 
   toggleImage(): void {
     this.showImage = !this.showImage;
   }
-  constructor() {
-    console.log('constructor called......');
-  }
 
   ngOnInit(): void {
-    this._listFilter = ' ';
+    this.products = this.productService.getProduct();
+    this.filteredProducts = this.products;
   }
-
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product List: ' + { message };
+  }
 }
-
