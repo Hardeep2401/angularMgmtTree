5b6527e0c01424bc97e58858d468746b9d64aec8
Hardeep2401 <hardeep.singh@lpinfotech.com>
2023-02-06 19:11:09 +0530

    add page currentMortgages

--- my-Angular/src/app/app-routing.module.ts
+++ my-Angular/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ApplicationContinuedComponent } from './application-continued/application-continued.component';
+import { CurrentMortgagesComponent } from './current-mortgages/current-mortgages.component';
+import { CardComponent } from './nav-card/card/card.component';
+import { NavCardComponent } from './nav-card/nav-card.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'navbar', component: NavCardComponent },
+  { path: 'home', component: CardComponent },
+  { path: 'about', component: ApplicationContinuedComponent },
+  { path: 'services', component: CurrentMortgagesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
