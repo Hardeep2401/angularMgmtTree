d4537195f343a576b35750c229235d42bf74fe40
Hardeep <hardeep.singh@lpinfotech.com>
2023-05-09 01:37:54 +0530

    update add slider image

--- my-Angular/src/app/server/server.component.ts
+++ my-Angular/src/app/server/server.component.ts
@@ -1,24 +1,26 @@
-import {
-  Component,
-  EventEmitter,
-  Input,
-  OnChanges,
-  Output,
-} from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css'],
 })
-export class ServerComponent implements OnChanges {
-  @Input() rating: number = 0;
-  cropWidth: number = 110;
-  @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
-  ngOnChanges(): void {
-    this.cropWidth = (this.rating * 110) / 5;
+export class ServerComponent  {
+  item: any;
+  constructor(private el : ElementRef){
   }
-  onClick(): void {
-    this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
-  }
+@HostListener('click')
+nextFun() {
+  // console.log(this.el.nativeElement)
+let elm = this.el.nativeElement.parentElement.parentElement.children[0];
+let item = elm.getElementsByClassName('item');
+elm.append(item[0]);
+}
+prev() {
+  // console.log(this.el.nativeElement)
+let elm = this.el.nativeElement.parentElement.parentElement.children[0];
+let item = elm.getElementsByClassName('item');
+elm.prepend(item[item.length - 1]);
+}
+
 }
